e9f4c7cd607f3785dd909c91c3c7fce593bd1bea
nyawiradev <njaginjoyce@gmail.com>
2022-07-23 01:14:05 +0300

    initial commit

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,35 +1,31 @@
 import React, { useState } from "react";
 
-function ItemForm() {
+function ItemForm({ onAddItem }) {
   const [name, setName] = useState("");
   const [category, setCategory] = useState("Produce");
 
+  function handleSubmit(e) {
+    e.preventDefault();
+    const itemData = {
+      name: name,
+      category: category,
+      isInCart: false,
+    };
+    fetch("http://localhost:4000/items", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(itemData),
+    })
+      .then((r) => r.json())
+      // call the onAddItem prop with the newItem
+      .then((newItem) => onAddItem(newItem));
+  }
   return (
-    <form className="NewItem">
-      <label>
-        Name:
-        <input
-          type="text"
-          name="name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-      </label>
-
-      <label>
-        Category:
-        <select
-          name="category"
-          value={category}
-          onChange={(e) => setCategory(e.target.value)}
-        >
-          <option value="Produce">Produce</option>
-          <option value="Dairy">Dairy</option>
-          <option value="Dessert">Dessert</option>
-        </select>
-      </label>
-
-      <button type="submit">Add to List</button>
+    // Set up the form to call handleSubmit when the form is submitted
+    <form className="NewItem" onSubmit={handleSubmit}>
+      {/** ...form inputs here */}
     </form>
   );
 }
